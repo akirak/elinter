73f503159e187c8944a69a61b46367e22e08377a
Akira Komamura <akira.komamura@gmail.com>
2019-12-14 08:06:46 +0900

    Use builtins.fetchTarball to prevent an undefined variable error

--- tests/emacs.nix
+++ tests/emacs.nix
@@ -1,7 +1,1 @@
-import (pkgs.fetchFromGitHub {
-  owner = "purcell";
-  repo = "nix-emacs-ci";
-  rev = "53e8f05a66addd520e1feec23eabd6a8a86ee47f";
-  # date = 2019-12-12T10:37:15+13:00;
-  sha256 = "0v524dsckbhn0y3ywj7dd1a74p6mi7gqz7xdpkzy3l8c2pvpy1sy";
-}).emacs-25-1
+(import (builtins.fetchTarball "https://github.com/purcell/nix-emacs-ci/archive/master.tar.gz")).emacs-25-1
