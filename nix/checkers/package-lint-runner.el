166e39ffe41cb985f7e3b8221ca997c8d10272dc
Akira Komamura <akira.komamura@gmail.com>
2020-05-10 19:00:59 +0900

    Fix: Add library header to package-lint-runner.el

--- nix/checkers/package-lint-runner.el
+++ nix/checkers/package-lint-runner.el
@@ -1,3 +1,35 @@
+;;; package-lint-runner.el --- Run package-lint in Nix -*- lexical-binding: t -*-
+
+;; Copyright (C) 2020 Akira Komamura
+
+;; Author: Akira Komamura <akira.komamura@gmail.com>
+;; Version: 0.1
+;; Package-Requires: ((emacs "25.1"))
+;; URL: https://github.com/akirak/melpa-check
+
+;; This file is not part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This script runs package-lint inside Nix.
+
+;;; Code:
+
 ;; The logic is based on an implementation in makel:
 ;; <https://gitlab.petton.fr/DamienCassou/makel/blob/master/makel.mk>
 
@@ -53,3 +85,7 @@
                 (list (cl-set-difference (car args) explicitly-installed-packages
                                          :test (lambda (x y) (eq (car x) y)))))))
 (package-lint-batch-and-exit)
+
+(provide 'package-lint-runner)
+;;; package-lint-runner.el ends here
+
