f6dae125fe57728e759364186727a01303c83cf0
Akira Komamura <akira.komamura@gmail.com>
2020-05-30 11:44:57 +0900

    Change shell.nix in cli directory to use local nixpkgs

--- cli/shell.nix
+++ cli/shell.nix
@@ -1,2 +1,8 @@
-let pkgs = import ../nix/pkgs.nix;
+let
+  sources = import ../nix/sources.nix;
+  overlay = import ../nix/overlay.nix;
+  pkgs = import <nixpkgs> {
+    overlays = [ overlay ];
+    config = { };
+  };
 in with pkgs; mkShell { buildInputs = [ spago nodejs purs gnumake ]; }
