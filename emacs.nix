f99ce39ab94d6532b28c491656f4e3a2b5168aad
Akira Komamura <akira.komamura@gmail.com>
2020-07-27 00:21:02 +0900

    Remove derivation for byte-compile (unused)

--- emacs.nix
+++ emacs.nix
@@ -10,8 +10,6 @@
 with builtins;
 with pkgs;
 let
-  gitignore = import (import ./nix/sources.nix).gitignore { inherit (pkgs) lib; };
-
   emacsForCI = emacsWithPackagesFromPackageRequires {
     package = pkgs."${emacs}";
     packageElisp = readFile (/. + mainFile);
@@ -28,49 +26,4 @@ in
       emacsForCI
     ];
   };
-
-  compile =
-    let
-      pname = getEnv "PACKAGE_NAME";
-      pwd = getEnv "PWD";
-      sourceFilesInString = getEnv "PACKAGE_ELISP_FILES";
-    in
-      stdenv.mkDerivation {
-        name = "elinter-${emacs}-package-${pname}";
-        version = "0";
-
-        src = gitignore.gitignoreSource (/. + pwd);
-
-        buildInputs = [ emacsForCI ];
-
-        phases = [ "buildPhase" "installPhase" ];
-
-        buildPhase = ''
-          result=0
-
-          ls -l $src
-
-          for f in ${sourceFilesInString}; do
-            cp $src/$f .
-            emacs --batch --no-site-file --eval "(setq byte-compile-error-on-warn t)" \
-              --funcall batch-byte-compile $f
-            if [[ $? -gt 0 ]]; then
-              result=1
-            fi
-          done
-
-          exit $result
-        '';
-
-        installPhase = ''
-          mkdir $out
-          cp -r $src/*.* $out
-          cp -r *.* $out
-          emacs --batch -Q --eval \
-             "(with-temp-buffer
-                (insert emacs-major-version)
-                 (write-region (point-min) (point-max) \"$out/.elinter-emacs-version\"))"
-        '';
-      };
-
 }
