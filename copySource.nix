c82348f7eaeb02489469c065a00d614c79ca1554
Akira Komamura <akira.komamura@gmail.com>
2020-08-06 21:53:52 +0900

    Add an option to clone a remote source repository

--- copySource.nix
+++ copySource.nix
@@ -1,5 +1,6 @@
 { pkgs ? import <nixpkgs> {}
 , src
+, remote ? false
 , recipe
 }:
 with builtins;
@@ -7,10 +8,49 @@ with pkgs;
 with (import (import ./nix/sources.nix).nix-elisp-helpers { inherit pkgs; });
 with (import (import ./nix/sources.nix).gitignore { inherit (pkgs) lib; });
 let
-  srcRoot = gitignoreSource (/. + "/${src}");
+  recipeAttrs = parseRecipe recipe;
+
+  remoteSrc =
+    with recipeAttrs;
+    let
+      path = split "/" repo;
+      owner = elemAt path 0;
+      repoPath = elemAt path 2;
+      repoAttrs = lib.filterAttrs (_: v: v != null) {
+        rev = commit;
+        ref = branch;
+      };
+      gitUrl =
+        if fetcher == "github"
+        then concatStringsSep "" [ "https://github.com/" owner "/" repoPath ".git" ]
+        else if fetcher == "gitlab"
+        then concatStringsSep "" [ "https://gitlab.com/" owner "/" repoPath ".git" ]
+        else if fetcher == "git"
+        then recipeAttrs.url or throw "url is required when you choose the git fetcher"
+        else null;
+      srcStore = if gitUrl != null
+      then fetchGit (
+        repoAttrs // {
+          name = pname;
+          url = gitUrl;
+        }
+      )
+      else if fetcher == "hg"
+      then fetchMercurial (
+        repoAttrs // {
+          inherit url;
+        }
+      )
+      else throw ("Unsupported fetcher type: " + fetcher);
+    in
+      lib.toDerivation srcStore;
+
+  srcRoot = if remote
+  then remoteSrc
+  else gitignoreSource (/. + "/${src}");
+
   package =
     let
-      recipeAttrs = parseRecipe recipe;
       isElisp = file: match ".+\.el" file != null;
       mainFileRegex = "(.*/)?" + recipeAttrs.pname + "\.el";
       isMainFile = file: match mainFileRegex file != null;
@@ -27,7 +67,8 @@ let
 in
   with package;
   let
-    packageFilePaths = map (file: src + "/" + file) packageFiles;
+    baseDir = if remote then toString remoteSrc else src;
+    packageFilePaths = map (file: baseDir + "/" + file) packageFiles;
     baseNames = xs: concatStringsSep " " (map baseNameOf xs);
   in
     writeText "emacs-${package.pname}-link-source"
