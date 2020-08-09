bcf59a036168d069cf6b91706bb45c8970f46ba0
Akira Komamura <akira.komamura@gmail.com>
2020-08-09 21:26:14 +0900

    Colorize terminal output from helpers

--- default.nix
+++ default.nix
@@ -39,18 +39,43 @@ rec {
 
     src = gitignoreSource ./.;
 
+    nativeBuildInputs = [ makeWrapper ];
+
     phases = [ "installPhase" ];
 
     installPhase =
       let
-        lint-runner = writeShellScript "elinter-run-linters" ''
+
+        lint-runner = writeShellScriptBin "elinter-run-linters" ''
           exec emacs -Q --batch --script ${./lisp/elinter-run-linters.el} "$@"
         '';
+
+        colorizer = writeShellScript "elinter-colorizer" ''
+           if command -v tput >/dev/null && [[ $(tput colors) -ge 8 ]]; then
+             ANSI_SUCCESS=$(tput setaf 2)$(tput bold)
+             ANSI_ERROR=$(tput setaf 1)$(tput bold)
+             ANSI_CAPTION=$(tput setaf 5)
+             ANSI_RESET=$(tput sgr0)
+             exec sed -e "s/\\(SUCCESS\\|OK\\)/''${ANSI_SUCCESS}\\1''${ANSI_RESET}/" \
+               -e "s/\\(FAIL\\(ED\\)\\?\\)/''${ANSI_ERROR}\\1''${ANSI_RESET}/" \
+               -e "s/^\\(Running\\|Compiling\\|Checking\\) .\\+\\.\\{3\\}$/''${ANSI_CAPTION}\\0''${ANSI_RESET}/"
+          else
+             exec cat
+           fi
+        '';
+
       in
         ''
-          mkdir -p $out/bin
-          cp $src/bin-helpers/* $out/bin
-          cp ${lint-runner} $out/bin/elinter-run-linters
+           mkdir -p $out/bin
+
+           # Install the colorizer
+           cp ${colorizer} $out/bin/elinter-colorizer
+
+           # Patch backend scripts to redirect output to the colorizer
+           for bin in $src/bin-helpers/* ${lint-runner}/bin/*; do
+             makeWrapper $bin $out/bin/`basename $bin` \
+               --run 'exec &> >(elinter-colorizer)'
+          done
         '';
   };
 
