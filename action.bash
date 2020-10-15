258cc40f857471393e2650839ea993d9f2e06038
Akira Komamura <akira.komamura@gmail.com>
2020-10-15 11:37:24 +0900

    Fix: Add missing -v option to executable existence check

--- action.bash
+++ action.bash
@@ -20,7 +20,7 @@ cachix use emacs-ci
 workflow_end_group
 
 workflow_start_group "Install elinter"
-if ! command elinter >/dev/null; then
+if ! command -v elinter >/dev/null; then
   nix-env -if https://github.com/akirak/elinter/archive/v4.tar.gz
 fi
 workflow_end_group
