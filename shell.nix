55b12211db879daf4015ea0bc119c36969f4b164
Akira Komamura <akira.komamura@gmail.com>
2021-07-03 07:21:38 +0900

    Add the formatters and linters

--- shell.nix
+++ shell.nix
@@ -1,5 +1,11 @@
 { pkgs ? import <nixpkgs> { } }:
 with (import ./nix/hooks.nix { inherit pkgs; });
 pkgs.mkShell {
+  buildInputs = with pkgs; [
+    nix-linter
+    nixpkgs-fmt
+    shellcheck
+  ];
+
   shellHook = pre-commit-check.shellHook;
 }
