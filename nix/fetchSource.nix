ef44bdcf7cb9e5841231d76753fc442d0ef8e6d4
Akira Komamura <akira.komamura@gmail.com>
2021-10-14 15:34:30 +0900

    Drop files that will be irrelevant in v5

--- nix/fetchSource.nix
+++
@@ -1,6 +0,0 @@
-# Download a remote source based on recipe
-{ pkgs ? import <nixpkgs> { }
-, recipeFile
-}:
-(import (import ./sources.nix).nix-elisp-helpers { inherit pkgs; }).fetchFromRecipe
-  (builtins.readFile recipeFile)
