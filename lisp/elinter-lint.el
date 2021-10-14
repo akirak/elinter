2034f31fbd8d6178d97f18e56e5f024407974983
Akira Komamura <akira.komamura@gmail.com>
2021-10-14 11:11:42 +0900

    Rename and add header

--- lisp/elinter-lint.el
+++ lisp/elinter-lint.el
@@ -1,4 +1,36 @@
-;; -*- lexical-binding: t -*-
+;;; elinter-lint.el --- Run multiple elisp linters in batch -*- lexical-binding: t -*-
+
+;; Copyright (C) 2021 Akira Komamura
+
+;; Author: Akira Komamura <akira.komamura@gmail.com>
+;; Version: 0.1
+;; Package-Requires: ((emacs "27.1"))
+;; Keywords:
+;; URL: https://github.com/akirak/elinter
+
+;; This file is not part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
 (require 'cl-lib)
 (require 'subr-x)
 
@@ -283,6 +315,5 @@ This function returns non-nil if there is any error found."
 
   (elinter-run-linters-and-exit))
 
-;; Local Variables:
-;; mode: emacs-lisp
-;; End:
+(provide 'elinter-lint)
+;;; elinter-lint.el ends here
